85db4f7ebfab608a646014839bfd5137e1c27ed3
shivaraja_vince <shivaraja@vince.no>
2020-01-16 16:24:53 +0530

    stripe server

---
+++ stripe-server/src/stripe.js
@@ -0,0 +1,27 @@
+const stripe = require("stripe")("sk_test_T0a4kiCJInFB1DjREcCAcg9l00GOHeag5g");
+
+async function postCharge(req, res) {
+  try {
+    const { amount, source, receipt_email } = req.body;
+
+    const charge = await stripe.charges.create({
+      amount,
+      currency: "usd",
+      source,
+      receipt_email
+    });
+
+    if (!charge) throw new Error("charge unsuccessful");
+
+    res.status(200).json({
+      message: "charge posted successfully",
+      charge
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+}
+
+module.exports = postCharge;
